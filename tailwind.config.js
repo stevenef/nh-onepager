c463aa696a125e1e5349ec79978df725f2eeb9e1
Steven Helmis <steven.helmis@scitotec.de>
2022-05-18 13:44:49 +0200

    initiales commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+	 	 extend: {
+			colors: {
+			"nh-grey-light": "#F3F3F3",
+			"nh-grey": "#868689",
+			"nh-grey-dark": "#616162",
+			"nh-orange-light": "#FFF7EA",
+			"nh-orange": "#F9B200",
+			"bookmark-purple": "#5267DF",
+			}
+	  	},
+	  	fontFamily: {
+			robotoflex: ["Roboto Flex", "sans-serif"],
+		},
+		container: {
+			center: true,
+			padding: "1rem",
+			screens: {
+				lg: "1024px",
+				xl: "1024px",
+				"2xl": "1024px"
+			}
+		}
+	},
+	plugins: [],
+  }
